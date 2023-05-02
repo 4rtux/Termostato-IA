1c6c0f2a22a3c54266856bab4ebf602380e1529a
100472007 <100472007@alumnos.uc3m.es>
2023-05-03 00:04:09 +0200

    revisión de errores y pruebas

--- termo.py
+++ termo.py
@@ -7,17 +7,20 @@ def bellman_ford(on_cost, off_cost, iterations):
     # Importar matrices de transicion de ON y OFF
     on_matrix = pd.read_csv('ON.csv', delimiter=',', index_col=0)
     off_matrix = pd.read_csv('OFF.csv', delimiter=',', index_col=0)
+
     # Las filas ya tienen indices del 0 al 18 pero las columnas tienen las temperaturas como indices
     # Trasponer las matrices, poner indices de 0 a 18 y volver a trasponer para que las columnas tengan indices del 0 al 18
     on_matrix= on_matrix.T.reset_index(drop=True).T
     off_matrix = off_matrix.T.reset_index(drop=True).T
-    
+    print('ON matrix: \n', on_matrix)
+    print('OFF matrix: \n', off_matrix)
+
     # Inicializar las listas de V_actual , V_anterior y la politica que guardaran los valores de V y la politica optima para cada estado
     num_states = len(on_matrix)
     policy = [0] * num_states
     V_previous = [0] * num_states
     V_current = [0] * num_states
-    
+
     # Calcular V para cada estado usando la ecuacion de Bellman
     while True:
         for state in range(num_states):
@@ -25,6 +28,7 @@ def bellman_ford(on_cost, off_cost, iterations):
             V_off = off_cost + sum([off_matrix[state][next_state] * V_previous[next_state] for next_state in range(num_states)])
             # Uso el round para redondear a 3 decimales, que agiliza el proceso y no supone grandes problemas de precision
             V_current[state] = min(round(V_on,3),round(V_off,3))
+            print(V_current)
             # Elegir la accion que minimiza el coste
             if V_on < V_off:
                 policy[state] = 1
@@ -40,7 +44,7 @@ def bellman_ford(on_cost, off_cost, iterations):
     # Empezando desde un estado aleatorio, seguir la politica optima hasta alcanzar la convergencia a 22 grados
     state = np.random.randint(0, num_states)
     n = 0
-    # Crear lista de temperaturas y de indices para poder saber a que temperatura corresponde cada estado
+    # Crear lista de temperaturas para poder saber a que temperatura corresponde cada estado
     # Los estados tienen indices del 0 al 18, pero las temperaturas van de 16 a 25.5 de 0.5 en 0.5
     # Esto se hace para que la representacion grafica sea mas intuitiva y contenga las temperaturas reales en lugar de los indices
     temperatures = []
@@ -69,8 +73,8 @@ def bellman_ford(on_cost, off_cost, iterations):
     return V_current, policy
 
 # Ejemplo de uso
-# Coste optimo es 4 en ON y 5 en OFF
-costo_on = 0.75
+# Coste optimo es 3 en ON y 4 en OFF
+costo_on = 1
 costo_off = 1
 
 valores_V = bellman_ford(costo_on, costo_off, 300)
